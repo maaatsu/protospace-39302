9fa0b2697e8c6c3de1686105427d2b00031b8d10
maaatsu <bigbear.honey.87@gmail.com>
2023-05-30 16:41:24 +0900

    マイグレーションファイル修正

--- db/migrate/20230510141401_create_prototypes.rb
+++ db/migrate/20230510141401_create_prototypes.rb
@@ -4,8 +4,7 @@ class CreatePrototypes < ActiveRecord::Migration[6.0]
       t.string :title,    null: false
       t.text :catch_copy, null: false
       t.text :concept,    null: false
-      t.references :user_id, null: false, foreign_key: true
-      add_reference :prototypes, :user, foreign_key: true
+      t.references :user, null: false, foreign_key: true
       t.timestamps
     end
   end
